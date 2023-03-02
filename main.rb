eed22f5b1e309d332f69375a5e8986c1811b2fbe
Mathias Soria <103398449+MathiasThoria@users.noreply.github.com>
2023-03-02 20:11:03 -0300

    First attempt to model data. Some Classes
    Board, Square and Coordenante class created. Some ideas to work around about how to see the board.

--- main.rb
+++ main.rb
@@ -1,1 +1,53 @@
 
+#Pawn / Bishop / kNight / Tower / Queen / King
+
+class Board  
+  def initialize()   
+    @square = []   
+    gets
+    7.times() do |x|
+      7.times() do |y|
+        sq =  Square.new(Coordenate.new(x,y), Piece.new("E") )    
+        p sq
+        @square << sq
+      end
+    end
+    
+  end
+end
+
+class Square
+  attr_accessor :coordenate, :content    
+  
+  def initialize(coordenate,content)
+      @coordenate = coordenate
+      @content = content
+  end
+  
+end
+
+class Coordenate
+  attr_accessor :x, :y
+  
+  def initialize(x,y)
+    @x=x
+    @y=y
+  end
+  
+  def to_arr
+    return [@x,@y]
+  end
+end  
+
+class Piece
+  attr_accessor :name
+  def initialize (name)
+    @name = name
+  end
+end      
+casilla = Square.new(Coordenate.new(1,2),"P")
+puts casilla.coordenate.x
+puts casilla.coordenate.y
+p casilla.coordenate.to_arr
+board_new=Board.new
+p board_new
