356a846d8b21901347e66a6cc1bbb5f93cec373a
Mathias Soria <103398449+MathiasThoria@users.noreply.github.com>
2023-03-02 21:28:27 -0300

    Print board. Some work on clases

--- main.rb
+++ main.rb
@@ -1,27 +1,58 @@
 
 #Pawn / Bishop / kNight / Tower / Queen / King
+#coordinate class its not working as intended. the idea is to reduce 
+#memory use. so many new coordinates quite the oposite
 
 class Board  
-  def initialize()   
-    @square = []   
-    gets
-    7.times() do |x|
-      7.times() do |y|
+  
+  def initialize()   #starts board with emty squares
+    @square = []      
+    8.times do |y|
+      8.times do |x|
         sq =  Square.new(Coordenate.new(x,y), Piece.new("E") )    
-        p sq
-        @square << sq
+        #print sq.coordenate.x 
+        #print sq.coordenate.y
+        #print sq.piece.name
+        #print "\n"
+        @square << sq        
+      end
+      #print "\n"
+    end    
+  end
+  
+  def show_board 
+    cont = 0
+    #@square.each {|sq| p sq.coordenate.x}
+    print cont
+    print " "
+    @square.each do |sq|        
+      if cont == 8 
+        print "\n" 
+        print sq.coordenate.y
+        print " "
+        cont = 0
+      end
+      if cont < 8
+        print sq.piece.name
+        print " "
+        cont += 1
       end
     end
-    
+    print "\n "
+    8.times {|n| print " #{n}"}
+    print "\n"
   end
 end
 
-class Square
-  attr_accessor :coordenate, :content    
+def change_piece(coordenate)  
   
+end
+
+class Square
+  attr_accessor :coordenate, :piece     
   def initialize(coordenate,content)
       @coordenate = coordenate
-      @content = content
+      @piece = content
   end
   
 end
@@ -45,9 +76,6 @@ class Piece
     @name = name
   end
 end      
-casilla = Square.new(Coordenate.new(1,2),"P")
-puts casilla.coordenate.x
-puts casilla.coordenate.y
-p casilla.coordenate.to_arr
-board_new=Board.new
-p board_new
+
+board_new = Board.new
+board_new.show_board
